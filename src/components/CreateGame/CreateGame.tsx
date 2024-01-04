6e7407af9ea327cebf335b0a773b818e57a8a428
Alexandre RICHARD <alexandre.richard.dev@gmail.com>
2024-01-04 19:37:03 +0100

    :construction: Input for chose playerNumber ready

--- src/components/CreateGame/CreateGame.tsx
+++ src/components/CreateGame/CreateGame.tsx
@@ -1,11 +1,64 @@
 import React from "react";
 
+import {
+    gameActions,
+    gameState,
+    useAppDispatch,
+    useAppSelector
+} from "@/IndexImporter";
 import "./CreateGame.scss";
 
 const CreateGame: React.FC = () => {
+    // Use the typed version create in hooks.ts
+    const dispatch = useAppDispatch();
+    const gameData = useAppSelector(gameState.GameData);
+
+    const changePlayerNumber = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        let inputValue = parseInt(event.target.value, 10);
+        if (isNaN(inputValue) || inputValue < 4 || inputValue > 49) {
+            inputValue = 8;
+        }
+        dispatch(gameActions.changePlayerNumber(inputValue));
+    };
+
+    const buttonChangePlayerNumber = (param: string) => {
+        const newValue = param === "pos"
+            ? gameData.playerNumber + 1
+            : gameData.playerNumber - 1;
+        dispatch(gameActions.changePlayerNumber(newValue));
+    };
+
     return (
         <>
-            CreateGame
+            <p>
+                1) Choisir le nombre de joueur
+            </p>
+            <div className="number-selector-container">
+                <button
+                    type="button"
+                    onClick={() => buttonChangePlayerNumber("neg")}
+                >
+                    -
+                </button>
+                <input
+                    max="49"
+                    min="4"
+                    type="range"
+                    value={gameData.playerNumber}
+                    onChange={changePlayerNumber}
+                />
+                <button
+                    type="button"
+                    onClick={() => buttonChangePlayerNumber("pos")}
+                >
+                    +
+                </button>
+            </div>
+            <p>
+                2) Choisir la composition
+            </p>
         </>
     );
 };
--- src/store/slices/gameSlice.ts
+++ src/store/slices/gameSlice.ts
@@ -4,9 +4,15 @@ import type {RootState} from "@/IndexImporter";
 
 interface GameState {
     appStep: string;
+    gameData: {
+        playerNumber: number;
+    };
 }
 
-const initialState: GameState = {"appStep": "starting"};
+const initialState: GameState = {
+    "appStep": "starting",
+    "gameData": {"playerNumber": 8},
+};
 
 const gameSlice = createSlice({
     "name": "game",
@@ -15,10 +21,16 @@ const gameSlice = createSlice({
         "changeAppStep": (state, action: PayloadAction<string>) => {
             state.appStep = action.payload;
         },
+        "changePlayerNumber": (state, action: PayloadAction<number>) => {
+            state.gameData.playerNumber = action.payload;
+        },
     },
 });
 
 // Export all state in one object to have only to imports : Action and State.
-export const gameState = {"AppStep": (state: RootState) => state.game.appStep};
+export const gameState = {
+    "AppStep": (state: RootState) => state.game.appStep,
+    "GameData": (state: RootState) => state.game.gameData,
+};
 export const gameActions = gameSlice.actions;
 export default gameSlice.reducer;
